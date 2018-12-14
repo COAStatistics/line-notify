2f357357c984a35d2c3d1d0689b6c39ac458c453
MatsuiLin101 <matsuilin101@gmail.com>
2018-12-14 15:48:44 +0800

    HttpResponseBadRequest

--- src/linenotify/views.py
+++ src/linenotify/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.conf import settings
 
 from linebot import LineBotApi, WebhookHandler
