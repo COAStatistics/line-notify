33f524e0c5da8870d546e134456b77b83f395f3e
MatsuiLin101 <matsuilin101@gmail.com>
2018-12-14 15:23:50 +0800

    Line bot reply echo

--- src/linenotify/views.py
+++ src/linenotify/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError,LineBotApiError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+line_bot_api = LineBotApi(settings.LINE_BOT_API)
+handler = WebhookHandler(settings.HANDLER)
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello')
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):
+                line_bot_api.reply_message(
+                    event.reply_token,
+                   TextSendMessage(text=event.message.text)
+                )
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
