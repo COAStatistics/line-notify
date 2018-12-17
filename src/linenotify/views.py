2a6b5980fbeb1fcebf3eb60ce9b42d37cea0ac92
MatsuiLin101 <matsuilin101@gmail.com>
2018-12-17 16:38:50 +0800

    Save userid

--- src/linenotify/views.py
+++ src/linenotify/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from . import models
 
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError,LineBotApiError
@@ -31,9 +32,21 @@ def callback(request):
 
         for event in events:
             if isinstance(event, MessageEvent):
+                if event.message.text == '吃大便' or event.message.text == '呱吉吃大便':
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text='恭喜答對，以後一起吃大便~耶!')
+                    )
+                    models.UserID.objects.create(user_id=event.source.userId)
+                else:
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text=event.message.text)
+                    )
+            if isinstance(event, FollowEvent):
                 line_bot_api.reply_message(
                     event.reply_token,
-                   TextSendMessage(text=event.message.text)
+                    TextSendMessage(text='請說出通關密語以接收最新消息')
                 )
         return HttpResponse()
     else:
